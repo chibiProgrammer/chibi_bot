e9f82bb0ef597276dad8b6b1a58e466291b9ea68
chibiProgrammer <nakamura_m@uuum.jp>
2020-03-07 14:42:30 +0900

    fix

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const app = express();
 app.get('/', (req, res)=> res.send("hello"));
 
 const port = 18000;
-app.listen(port, () => console.log("good"))
+app.listen(port, () => console.log("good!!!!!"))
