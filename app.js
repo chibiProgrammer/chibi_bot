417a4e4900e8227bbbe50425070000f32f85c8c0
takeokunn <bararararatty@gmail.com>
2020-03-07 14:56:56 +0900

    Change port

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const express = require("express");
 const app = express();
 app.get('/', (req, res)=> res.send("hello"));
 
-const port = 18000;
+const port = process.env.PORT || 18000;
 app.listen(port, () => console.log("good!!!!!"))
